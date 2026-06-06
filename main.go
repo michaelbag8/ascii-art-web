6ed2a3f2e29b11309d1b0767f9466dfff4163762
michaelbag8 <michaelbag8@gmail.com>
2026-06-06 18:18:30 +0100

    fix: error on exact path match

--- main.go
+++ main.go
@@ -8,11 +8,13 @@ import (
 
 func main() {
 
-	fmt.Println("server is runing.....http://localhost:8080")
-	http.HandleFunc("GET /", homeHandler)
+	http.HandleFunc("GET /{$}", homeHandler)
 	http.HandleFunc("POST /ascii-art", asciiArtHandler)
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+
+	fmt.Println("server is runing.....http://localhost:8080")
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
