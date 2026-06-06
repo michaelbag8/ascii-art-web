6d7460b03ab8cbef8aa5e56042111c53f665ff94
michaelbag8 <michaelbag8@gmail.com>
2026-06-06 18:03:58 +0100

    code refactored

--- handler.go
+++ handler.go
@@ -14,14 +14,6 @@ type PageData struct {
 var temp = template.Must(template.ParseFiles("templates/index.html"))
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/"{
-		renderError(w, http.StatusMethodNotAllowed, "Method Not Allowed")
-		return
-	}
-	if r.Method != http.MethodGet {
-		renderError(w, http.StatusMethodNotAllowed, "Method Not Allowed")
-		return
-	}
 
 	err := temp.Execute(w, nil)
 	if err != nil {
@@ -32,33 +24,14 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func asciiArtHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		renderError(w, http.StatusMethodNotAllowed, "Method Not Allowed")
-		return
-
-	}
+	
 	banner := r.FormValue("banner")
 	input := r.FormValue("input")
 
-	if input == "" {
+	err :=ValidateInput(input, banner)
+	if err!=nil {
 		renderError(w, http.StatusBadRequest, "Bad Request")
 		return
-	}
-	if banner != "standard" && banner != "shadow" && banner != "thinkertoy" {
-		renderError(w, http.StatusInternalServerError, "empty banner")
-		return
-	}
-	for _, ch := range input {
-		if ch == '\n' {
-			continue
-		}
-		for _, ch := range input {
-			if ch < 32 || ch > 126 {
-				renderError(w, http.StatusInternalServerError, "Non ascii character")
-				return
-			}
-		}
-
 	}
 
 	bannerMap, err := LoadBanner("banner/" + banner + ".txt")
--- main.go
+++ main.go
@@ -9,8 +9,8 @@ import (
 func main() {
 
 	fmt.Println("server is runing.....http://localhost:8080")
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/ascii-art", asciiArtHandler)
+	http.HandleFunc("GET /", homeHandler)
+	http.HandleFunc("POST /ascii-art", asciiArtHandler)
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
